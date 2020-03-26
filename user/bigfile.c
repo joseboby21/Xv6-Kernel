6caacb45a80e223be0fd8432faeb7f1bba45a9a7
Boby Jose <joseboby21@gmail.com>
2020-03-25 22:02:07 -0400

    test for itrunc added

--- user/bigfile.c
+++ user/bigfile.c
@@ -34,6 +34,113 @@ main()
   }
   
   close(fd);
+
+  fd = open("big.file1", O_CREATE | O_WRONLY);
+  if(fd < 0){
+    printf("bigfile: cannot open big.file for writing\n");
+    exit(-1);
+  }
+
+  blocks = 0;
+  while(1){
+    *(int*)buf = blocks;
+    int cc = write(fd, buf, sizeof(buf));
+    if(cc <= 0)
+      break;
+    blocks++;
+    if (blocks % 100 == 0)
+      printf(".");
+  }
+
+  printf("\nwrote %d blocks\n", blocks);
+  if(blocks != 65803) {
+    printf("bigfile1: file is too small\n");
+    exit(-1);
+  }
+  close(fd);
+  unlink("big.file1");
+
+
+  fd = open("big.file2", O_CREATE | O_WRONLY);
+  if(fd < 0){
+    printf("bigfile: cannot open big.file for writing\n");
+    exit(-1);
+  }
+
+  blocks = 0;
+  while(1){
+    *(int*)buf = blocks;
+    int cc = write(fd, buf, sizeof(buf));
+    if(cc <= 0)
+      break;
+    blocks++;
+    if (blocks % 100 == 0)
+      printf(".");
+  }
+
+  printf("\nwrote %d blocks\n", blocks);
+  if(blocks != 65803) {
+    printf("bigfile2: file is too small\n");
+    exit(-1);
+  }
+  close(fd);
+  unlink("big.file2");
+  
+
+  fd = open("big.file3", O_CREATE | O_WRONLY);
+  if(fd < 0){
+    printf("bigfile: cannot open big.file for writing\n");
+    exit(-1);
+  }
+
+  blocks = 0;
+  while(1){
+    *(int*)buf = blocks;
+    int cc = write(fd, buf, sizeof(buf));
+    if(cc <= 0)
+      break;
+    blocks++;
+    if (blocks % 100 == 0)
+      printf(".");
+  }
+
+  printf("\nwrote %d blocks\n", blocks);
+  if(blocks != 65803) {
+    printf("bigfile3: file is too small\n");
+    exit(-1);
+  }
+  close(fd);
+  unlink("big.file3");
+
+
+
+  fd = open("big.file4", O_CREATE | O_WRONLY);
+  if(fd < 0){
+    printf("bigfile: cannot open big.file for writing\n");
+    exit(-1);
+  }
+
+  blocks = 0;
+  while(1){
+    *(int*)buf = blocks;
+    int cc = write(fd, buf, sizeof(buf));
+    if(cc <= 0)
+      break;
+    blocks++;
+    if (blocks % 100 == 0)
+      printf(".");
+  }
+
+  printf("\nwrote %d blocks\n", blocks);
+  if(blocks != 65803) {
+    printf("bigfile4: file is too small\n");
+    exit(-1);
+  }
+  close(fd);
+  unlink("big.file4");
+  
+  
+  
   fd = open("big.file", O_RDONLY);
   if(fd < 0){
     printf("bigfile: cannot re-open big.file for reading\n");
